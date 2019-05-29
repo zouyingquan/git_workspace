46195d6158b8edb393b9b8229e93f959412a782c
zouzouhuihui <136081034@qq.com>
2019-05-29 16:15:51 +0800

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -95,7 +95,7 @@ int main()
 	//   2     3
 	// 4   5 6   7
 	
-	TreeNode<int> *node1 = new TreeNode<int>(1);
+    TreeNode<int> *node1 = new TreeNode<int>(1);
     TreeNode<int> *node2 = new TreeNode<int>(2);
     TreeNode<int> *node3 = new TreeNode<int>(3);
     TreeNode<int> *node4 = new TreeNode<int>(4);
